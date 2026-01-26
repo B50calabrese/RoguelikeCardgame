361751aa1bdb50ae86b9a193a334b24b0824f73d
Antonio Calabrese <antoniobrese@gmail.com>
2026-01-26 05:36:16 -0500

    Initial set of changes

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include "application.h"
+#include "engine.h"
+#include "scene.h"
+#include "scene_manager.h"
+
+// --- A Simple Game Scene ---
+class GameScene : public engine::Scene {
+ public:
+  GameScene() : Scene("MainGame") {}
+
+  void OnAttach() override { std::cout << "Game Scene Attached" << std::endl; }
+
+  void OnUpdate(float dt) override {
+    // Game logic (AI, Card movement) goes here
+  }
+
+  void OnRender() override {}
+};
+
+class DeckBuilderApp : public engine::Application {
+ public:
+  DeckBuilderApp() = default;  // Add a default constructor
+
+  void OnInit() override {
+    // Push the first scene
+    engine::SceneManager::Get().SetScene(std::make_unique<GameScene>());
+  }
+
+  void OnUpdate(double deltaTimeSeconds) override {}
+
+  void OnShutdown() override {
+    std::cout << "Game cleaning up..." << std::endl;
+  }
+};
+
+// --- Entry Point ---
+int main() {
+  engine::EngineConfig engine_config;
+  engine_config.window_width = 1600;
+  engine_config.window_height = 1200;
+  engine::Engine::Init(engine_config);
+  DeckBuilderApp app;
+  app.Run();
+  return 0;
+}
